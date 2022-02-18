dd952e527447eac1f51dc2cff26caf06d87722d2
Pradeep Kumar <pk6361439@gmail.com>
2022-02-18 22:42:18 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,309 @@
+var arr=[
+    {
+        "college_name": "Hansraj College Delhi University - 1",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 2",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 3",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 4",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 5",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 6",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 7",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 8",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 7",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 8",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 7",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 8",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    }, {
+        "college_name": "Hansraj College Delhi University - 7",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    },
+    {
+        "college_name": "Hansraj College Delhi University - 8",
+        "promoted": true,
+        "discount": 20,
+        "original_fees": 6000,
+        "discounted_fees": 5200,
+        "fees_cycle": "Per Semester (3months)",
+        "image": "college.png",
+        "ranking": "7 in 260 colleges in north campus",
+        "tags": ["best colleges in 2018", "2kms away"],
+        "amenties": ["Fee cancellation", "Free Wifi"],
+        "rating": 4,
+        "rating_remarks": "Very Good",
+        "famous_nearest_places": "2.5kms from GTB Nagar, 7 Kms from Rajiv Chowk",
+        "nearest_place": ["Near Vishwavidyalya Metro Station", "2 Kms away from bus stand"],
+        "offertext": "Flat Rs2,000 off + upto Rs 500 wallet! to avail... LOGIN "
+    }
+
+
+
+
+
+
+
+
+]
+function fetch_json(){
+    fetch('colleges.json')
+    .then((res)=>{
+        return res.json();
+       
+    }).then((data)=>{
+        // console.log(data);
+        arr=data;
+       
+    })
+}
+fetch_json();
+
+function Display(){
+    console.log(arr);
+    const dis=document.getElementById('display');
+    arr.map((e)=>{
+        console.log(e);
+        console.log("welcome")
+        const html=document.createElement('div');
+        html.className='flex_show'
+        html.innerHTML=`
+        <div class="container">
+        <div class="banner">
+            <img src="college_02.jpg" class="img">
+        </div>
+        <div class="promoted">
+        <p>Promoted</p>
+        </div>
+        <div class="Rating">
+            <p>${e.rating}/5</p>
+            <p>${e.rating_remarks}</p>
+        </div>
+        <div class="college_informaion">
+        <div class="college_desc">${e.tags[0]}</div>
+        <div class="college_dir">${e.tags[1]}</div>
+        <div class="ranking">${e.ranking}</div>
+        </div>
+        <div class="more_inf">
+            <div class="more_info1">
+                <h2>${e.college_name}</h2>
+                <h4>${e.nearest_place[0]}<span class="blur">|${e.nearest_place[1]}</h4>
+                <h4><span class="green">93% Match : <span class="blur">${e.famous_nearest_places}</span></h4>
+                 <div class="offer">
+                     <h4>${e.offertext}</h4>
+                 </div>
+                </div>
+            <div class="more_info2">
+              <h3>Original Price</h3>
+              <h3 class="blur">${e.original_fees}</h3>
+              <h3>Discounted  Price</h3>
+              <h3 class="red">${e.discounted_fees}</h3>
+              <h4 class="blur">${e.fees_cycle}</h4>
+
+            </div>
+        </div>
+    </div>
+        `
+        dis.appendChild(html)
+    })
+}
+Display();
